b8b5b4ba4f5a028b66c409fc541b93d9e27f8ce7
dawosch <57100280+dawosch@users.noreply.github.com>
2024-12-18 15:18:11 +0100

    keymap updated

--- sofle/keymaps/custom/keymap.c
+++ sofle/keymaps/custom/keymap.c
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_NO, KC_NO,
-                      KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                  KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
     [_FKEYS] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO,KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,
-                      KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                  KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
     [_SETUP] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG,
