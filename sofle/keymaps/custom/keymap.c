c7765e3880da2e33bbaab22c499f9c1f9b9b3518
Dawosch <57100280+dawosch@users.noreply.github.com>
2025-03-31 09:10:10 +0200

    Add files via upload

--- sofle/keymaps/custom/keymap.c
+++ sofle/keymaps/custom/keymap.c
@@ -5,51 +5,51 @@
 #include "raw_hid.h"
 
 enum layers {
-    _NOTED = 0,
-    _QWERTZ,
-    _SYMBOLS,
-    _NUMPAD,
-    _FKEYS,
-    _SETUP,
+    _CNRY = 0,
+    _QWRTZ,
+    _SYMBLS,
+    _NMPD,
+    _NVGTN,
+    _STP,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_NOTED] = LAYOUT(
-        LT(_SETUP, KC_ESC), DE_1, DE_2, DE_3, DE_4, DE_5,                               DE_6, DE_7, DE_8, DE_9, DE_0, KC_BSPC,
-        KC_TAB, DE_Z, DE_Y, DE_U, DE_A, DE_Q,                                           DE_P, DE_B, DE_M, DE_L, DE_F, DE_J,
-        KC_LSFT, DE_C, DE_S, DE_I, DE_E, DE_O,                                          DE_D, DE_T, DE_N, DE_R, DE_H, DE_SS,
-        KC_LCTL, DE_V, DE_X, DE_UDIA, DE_ADIA, DE_ODIA, KC_MUTE,                    DF(_QWERTZ), DE_W, DE_G, DE_COMM, DE_DOT, DE_K, DE_GRV,
-                KC_LALT, KC_LGUI, MO(_FKEYS), MO(_NUMPAD), LCTL_T(KC_SPC),          KC_ENT, MO(_SYMBOLS), KC_NO, KC_NO, KC_NO),
+    [_CNRY] = LAYOUT(
+        KC_ESC, DE_1, DE_2, DE_3, DE_4, DE_5,                                           DE_6, DE_7, DE_8, DE_9, DE_0, KC_BSPC,
+        KC_TAB, DE_W, DE_L, DE_Y, DE_P, DE_B,                                           DE_Z, DE_F, DE_O, DE_U, DE_ODIA, DE_ADIA,
+        KC_LSFT, DE_C, DE_R, DE_S, DE_T, DE_G,                                          DE_M, DE_N, DE_E, DE_I, DE_A, DE_UDIA,
+        KC_LCTL, DE_Q, DE_J, DE_V, DE_D, DE_K, DF(_STP),                            DF(_QWRTZ), DE_X, DE_H, DE_SS, DE_COMM, DE_DOT, KC_RSFT,
+                KC_LALT, KC_LGUI, MO(_NVGTN), MO(_NMPD), LCTL_T(KC_SPC),            KC_ENT, MO(_SYMBLS), KC_NO, KC_NO, KC_NO),
 
-    [_QWERTZ] = LAYOUT(
-        LT(_SETUP, KC_ESC), DE_1, DE_2, DE_3, DE_4, DE_5,                               DE_6, DE_7, DE_8, DE_9, DE_0, KC_BSPC,
+    [_QWRTZ] = LAYOUT(
+        KC_ESC, DE_1, DE_2, DE_3, DE_4, DE_5,                                           DE_6, DE_7, DE_8, DE_9, DE_0, KC_BSPC,
         KC_TAB, DE_Q, DE_W, DE_E, DE_R, DE_T,                                           DE_Z, DE_U, DE_I, DE_O, DE_P, DE_UDIA,
         KC_LSFT, DE_A, DE_S, DE_D, DE_F, DE_G,                                          DE_H, DE_J, DE_K, DE_L, DE_ODIA, DE_ADIA,
-        KC_LCTL, DE_Y, DE_X, DE_C, DE_V, DE_B, KC_NO,                               DF(_NOTED), DE_N, DE_M, DE_COMM, DE_DOT, DE_MINS, KC_RSFT,
-                MO(_FKEYS), MO(_NUMPAD), KC_LGUI, KC_LALT, LT(_SYMBOLS, KC_SPC),    KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
+        KC_LCTL, DE_Y, DE_X, DE_C, DE_V, DE_B, DF(_STP),                            DF(_CNRY), DE_N, DE_M, DE_COMM, DE_DOT, DE_MINS, KC_RSFT,
+                KC_LALT, KC_LGUI, MO(_NVGTN), MO(_NMPD), LCTL_T(KC_SPC),            KC_ENT, MO(_SYMBLS), KC_NO, KC_NO, KC_NO),
 
-    [_SYMBOLS] = LAYOUT(
+    [_SYMBLS] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_TRNS,
         KC_TRNS, DE_QUOT, DE_AMPR, KC_NO, DE_DQUO, DE_DOT,                              DE_LABK, DE_LPRN, DE_LBRC, DE_LCBR, DE_PERC, KC_NO,
         KC_TRNS, DE_EXLM, KC_PMNS, KC_PPLS, DE_EQL, KC_NUHS,                            DE_RABK, DE_RPRN, DE_RBRC, DE_RCBR, DE_QUES, KC_NO,
-        KC_TRNS, DE_CIRC, DE_SLSH, DE_ASTR, KC_BSLS, KC_NO, KC_NO,                  KC_NO, DE_TILD, DE_DLR, DE_PIPE, KC_NO, KC_AT, KC_NO,
+        KC_TRNS, DE_CIRC, DE_SLSH, DE_ASTR, KC_BSLS, KC_NO, KC_NO,                  KC_NO, DE_TILD, DE_DLR, DE_PIPE, DE_GRV, KC_AT, KC_NO,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    [_NUMPAD] = LAYOUT(
+    [_NMPD] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                     KC_NO, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                     KC_NO, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_NO, KC_NO,
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,                                     KC_NO, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO,
+        KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,                                     KC_NO, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO,
+        KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,                       KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_NO, KC_NO,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    [_FKEYS] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,                                     KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
-        KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,                                     KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO,
-        KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [_NVGTN] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
 
-    [_SETUP] = LAYOUT(
+    [_STP] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       RGB_HUD, RGB_HUI, KC_NO, KC_NO, RGB_RMOD, RGB_MOD,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       RGB_SAD, RGB_SAI, KC_NO, KC_NO, RGB_SPD, RGB_SPI,
@@ -98,34 +98,34 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 static void print_status_narrow(void) {
     switch (get_highest_layer(default_layer_state)) {
-        case _QWERTZ:
+        case _CNRY:
+            oled_write(PSTR("CNRY\n"), false);
+            break;
+        case _QWRTZ:
             oled_write(PSTR("QWRTZ\n"), false);
             break;
-        case _NOTED:
-            oled_write(PSTR("NOTED\n"), false);
-            break;
         default:
-            oled_write_ln_P(PSTR("UNDEF"), false);
+            oled_write(PSTR("UNDEF\n"), false);
     }
     oled_write_P(PSTR("-----"), false);
     oled_write_P(PSTR("\n\n"), false);
 
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
-        case _QWERTZ:
-        case _NOTED:
+        case _CNRY:
+        case _QWRTZ:
             oled_write_P(PSTR("Base\n"), false);
             break;
-        case _SYMBOLS:
+        case _SYMBLS:
             oled_write_P(PSTR("Symbl\n"), false);
             break;
-        case _NUMPAD:
+        case _NMPD:
             oled_write_P(PSTR("Nmpd\n"), false);
             break;
-        case _FKEYS:
-            oled_write_P(PSTR("FKeys\n"), false);
+        case _NVGTN:
+            oled_write_P(PSTR("Nvgtn\n"), false);
             break;
-        case _SETUP:
+        case _STP:
             oled_write_P(PSTR("Setup\n"), false);
             break;
         default:
